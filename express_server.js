64c5e7ed0223d13caf7eb82b135644538c99ccd2
Youssef Nafaa <nafaayoussef@gmail.com>
2021-11-13 02:11:50 +0000

    Add file to communicate with server

--- express_server.js
+++ express_server.js
@@ -1,22 +1,29 @@
+/**
+ * Using express to communicate between client and server
+ */
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-// Object simulating a database to work on
+/**
+ * Object simulating a database to work on
+ */
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/test/hello", (req, res) => {
-  res.send("Hello! from /test/hello");
-});
-
-
+app.get("/urls.j", (req, res) =>{
+  res.json(urlDatabase);
+})
+/**
+ * Listen on provided port
+ */
 app.listen(PORT, () => {
   console.log(`app listening on port ${PORT}!`);
 });
